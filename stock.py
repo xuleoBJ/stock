931a285842f0ebef2e93ec8d4d7f0506908c9582
xuleoBJ <xuleoBJ@gmail.com>
2015-05-04 21:06:13 +0800

    20150405pm2100

--- stock.py
+++ stock.py
@@ -78,6 +78,7 @@ def findPeak(days):
     print('分析周期(天):'+str(days))
     lineWrited.append('-'*50)
     lineWrited.append('分析周期(天):'+str(days))
+    lineWrited.append("日期"+"\t局部高点/低点:\t"+"\t距上次峰值交易日个数:\t"+"\t浮动幅度%:\t")
     d1=convertDateStr2Date(shDateStrList[0])
     d2=convertDateStr2Date(shDateStrList[0])
     standValue=100
@@ -89,7 +90,7 @@ def findPeak(days):
         if max_index==i:
             d2=convertDateStr2Date(shDateStrList[i])
             daysSpan=(d2-d1).days
-            lineWrited.append(shDateStrList[i]+"\t局部高点:\t"+str(shPriceHighestFList[i])+"\t距上次峰值交易日个数:\t"+str(daysSpan)+"\t浮动幅度%:\t"+str(round((max_value-standValue)/standValue,3)*100))
+            lineWrited.append(shDateStrList[i]+"\t"+str(shPriceHighestFList[i])+"\t"+str(daysSpan)+"\t"+str(round((max_value-standValue)/standValue,3)*100))
             d1=d2
             standValue=max_value
            
@@ -98,12 +99,13 @@ def findPeak(days):
         if min_index==i:
             d2=convertDateStr2Date(shDateStrList[i])
             daysSpan=(d2-d1).days
-            lineWrited.append(shDateStrList[i]+"\t局部低点:\t"+str(shPriceLowestFList[i])+"\t距上次峰值交易日个数:\t"+str(daysSpan)+"\t浮动幅度%:\t"+str(round((min_value-standValue)/standValue,3)*100))
+            lineWrited.append(shDateStrList[i]+"\t"+str(shPriceLowestFList[i])+"\t"+str(daysSpan)+"\t"+str(round((min_value-standValue)/standValue,3)*100))
             d1=d2
             standValue=min_value
     d2=convertDateStr2Date(shDateStrList[-1])
     daysSpan=(d2-d1).days
-    lineWrited.append(shDateStrList[-1]+"\t当前点位:\t"+str(shPriceCloseingFList[-1])+"\t距离上次峰值交易日个数:\t"+str(daysSpan)+"\t浮动幅度%:\t"+str(round((shPriceCloseingFList[-1]-standValue)/standValue,3)*100))
+    lineWrited.append(shDateStrList[-1]+"\t"+str(shPriceCloseingFList[-1])+"\t"+str(daysSpan)+"\t"+str(round((shPriceCloseingFList[-1]-standValue)/standValue,3)*100))
+    
 
 def readStockByID(stockID):
     print("\n"+"#"*80)
@@ -195,6 +197,31 @@ def analysisConsecutive(stockID,dateStrStart,dateStrEnd,numConsecutiveTradeDays,
     for item in waveFList:
         strDate=strDate+dateStrList[item]+"\t"
     print("振幅超过比例天数:\t"+str(len(waveFList))+"\t起始日期是："+strDate)
+
+##分析股票与大盘走势的同步性
+def analysisSynchronization(stockID,dateStrStart,dateStrEnd):
+## get analysis indexStartDay and indexEndDay by dateStrList
+    indexStart=dateStrList.index(dateStrStart)
+    indexEnd=dateStrList.index(dateStrEnd)
+    print("-"*50)
+    print("与大盘同步性分析")
+    synFile=stockID+"syn.txt"
+    fileWrited=open(synFile,'w')
+    synFList=[] ## 波动幅度
+## 通过日期找到大盘同期index
+    fileWrited.write("日期"+"\t"+"大盘涨幅"+"\t"+"股票涨幅"+"\t"+ '同步比例\n')
+    for i in range(indexStart,indexEnd):
+        dateStrSH=dateStrList[i]
+        indexSH=shDateStrList.index(dateStrSH)
+        r1=(priceCloseingFList[i]-priceOpeningFList[i])/priceCloseingFList[i-1]
+        rSH=(shPriceCloseingFList[indexSH]-shPriceOpeningFList[indexSH])/shPriceCloseingFList[indexSH-1]
+        synFList.append(r1/rSH)
+        line=dateStrSH+"\t"+str(rSH)+"\t"+str(r1)+"\t"+ str(round(r1/rSH,2))
+        fileWrited.write(line+'\n')
+    fileWrited.close()
+    print("大盘同步性分析写入"+synFile)
+    
+
     
 
 def highAndlowPrice(stockID,daysList):
@@ -240,7 +267,7 @@ if __name__=="__main__":
     findPeak(100)
     findPeak(200) 
 
-    stockID="600196"
+    stockID="002673"
     readStockByID(stockID)
   ##  highAndlowPrice(stockID,[iDaysPeriodUser,30,60,120])
     for item in [iDaysPeriodUser,30,60,120]:
@@ -257,7 +284,8 @@ if __name__=="__main__":
     for line in lineWrited:
         fileWrited.write(line+'\n')
     fileWrited.close()
-
+    
+    analysisSynchronization(stockID,dateStrList[-100],dateStrList[-1])
     timeSpan=time.clock()-startClock
 
     print("Time used(s):",round(timeSpan,2))
